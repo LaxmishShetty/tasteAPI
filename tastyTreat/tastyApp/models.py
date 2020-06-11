b3cd7a9361f0e56471439208227f49c5c7585e64
LaxmishShetty <shettylaxmish26@gmail.com>
2020-06-11 23:10:13 +0530

    "Checking commits"

--- tastyTreat/tastyApp/models.py
+++ tastyTreat/tastyApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Taste(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=40)
+    tasteOfDish = models.ForeignKey(Taste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class PersonName(models.Model):
+    name = models.CharField(max_length=50)
+    heCanEat = models.ManyToManyField(Dish)
+
+    def __str__(self):
+        return self.name
+
